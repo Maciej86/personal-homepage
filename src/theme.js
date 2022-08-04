1cb91baf265084598d14885cb5bfc5a086f712ce
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-08-04 23:29:24 +0200

    Rename color

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ export const ThemeLight = {
     background: "rgb(251,251,254)",
     white: "rgb(255,255,255)",
     mineShaft: "rgb(37,37,37)",
-    salateGray: "rgb(110,126,145)",
+    slateGray: "rgb(110,126,145)",
     scienceBlue: "rgb(3,102,214)",
     iron: "rgba(289,213,218, .3)",
     shadowTile: "rgba(9, 10, 51, 0.1)",
