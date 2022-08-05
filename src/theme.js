b4eda56fb42096a911da08cf2f2ce6ca6c514174
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-08-05 21:54:05 +0200

    Changed color

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ export const ThemeLight = {
     mineShaft: "rgb(37,37,37)",
     slateGray: "rgb(110,126,145)",
     scienceBlue: "rgb(3,102,214)",
-    iron: "rgba(289,213,218, .3)",
+    iron: "rgba(209,213,218, .3)",
     shadowTile: "rgba(9, 10, 51, 0.1)",
   },
   radius: "4px",
