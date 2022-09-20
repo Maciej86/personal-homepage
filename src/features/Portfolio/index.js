61ef07e40ed1b662aa6fcc6ec67d8fdc0a9b7e6e
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-09-20 23:57:42 +0200

    Change of path slice

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { SmallTile } from "../../common/SamllTile/index";
-import { fetchRepoGithub, selectRepoLoading, selectRepoState } from "./slice";
+import { fetchRepoGithub, selectRepoLoading, selectRepoState } from "../slice";
 import {
   Wrapper,
   WrapperTile,
