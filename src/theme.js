4f98c326b5dd0d4959ecaa79265470eee195e1ac
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-09-20 22:45:42 +0200

    Added dark theme

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,27 @@
 export const ThemeLight = {
   color: {
     background: "rgb(251,251,254)",
+    backgroundTile: "rgb(255,255,255)",
     white: "rgb(255,255,255)",
     mineShaft: "rgb(37,37,37)",
     slateGray: "rgb(110,126,145)",
     scienceBlue: "rgb(3,102,214)",
     iron: "rgba(209,213,218, .3)",
     shadowTile: "rgba(9, 10, 51, 0.1)",
+  },
+  radius: "4px",
+};
+
+export const ThemeDark = {
+  color: {
+    background: "rgb(37,37,37)",
+    backgroundTile: "rgba(54, 54, 54, 0.72)",
+    white: "rgba(255,255,255);",
+    mineShaft: "rgb(235,235,235)",
+    slateGray: "rgb(235,235,235)",
+    scienceBlue: "rgb(33,136,255)",
+    iron: "rgba(209, 213, 218, 0.1)",
+    shadowTile: "rgba(9, 10, 51, 0.02)",
   },
   radius: "4px",
 };
