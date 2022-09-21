9744e10b04fd4c85df998b2e827f3e0f915cf4ed
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-09-21 23:32:37 +0200

    Change the theme

--- src/features/Theme/index.js
+++ src/features/Theme/index.js
@@ -1,8 +1,22 @@
-import { useDispatch } from "react-redux";
-import { toggleTheme } from "../slice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTheme, toggleTheme } from "../slice";
+import { Wrapper, Text, Button, Span, Img } from "./styled";
+import LightTheme from "../../common/icons/on_light.svg";
+import DarkTheme from "../../common/icons/on_dark.svg";
 
 export const Theme = () => {
+  const theme = useSelector(selectTheme);
+  console.log(theme);
   const dispatch = useDispatch();
 
-  return <button onClick={() => dispatch(toggleTheme())}>Przełącz</button>;
+  return (
+    <Wrapper>
+      <Text>DARK MODE {theme ? "ON" : "OFF"}</Text>
+      <Button onClick={() => dispatch(toggleTheme())}>
+        <Span toggleTheme={theme}>
+          <Img src={theme ? DarkTheme : LightTheme} alt="" />
+        </Span>
+      </Button>
+    </Wrapper>
+  );
 };
---
+++ src/features/Theme/styled.js
@@ -0,0 +1,47 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  text-align: right;
+`;
+
+export const Text = styled.p`
+  font-size: 12px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.slateGray};
+  text-transform: uppercase;
+`;
+
+export const Button = styled.button`
+  width: 47px;
+  height: 27px;
+  margin-left: 10px;
+  background: ${({ theme }) => theme.color.iron};
+  border: 1px solid ${({ theme }) => theme.color.slateGray};
+  border-radius: 25px;
+  cursor: pointer;
+`;
+
+export const Span = styled.span`
+  display: inline-block;
+  margin-left: 0;
+  padding: 1px 4px;
+  background: ${({ theme }) => theme.color.slateGray};
+  border-radius: 50%;
+  transform: translateX(-9px);
+  transition: transform 0.5s, background 0.5s;
+
+  ${({ toggleTheme }) =>
+    toggleTheme &&
+    css`
+      background: ${({ theme }) => theme.color.white};
+      transform: translateX(8px);
+    `}
+`;
+
+export const Img = styled.img`
+  width: 14px;
+  height: 14px;
+`;
