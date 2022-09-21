3180f1b79bbf248ed120b0371e15b9c8f1ae243b
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-09-21 23:35:27 +0200

    Start theme light

--- src/features/Theme/themeLocalStorage.js
+++ src/features/Theme/themeLocalStorage.js
@@ -4,4 +4,4 @@ export const saveThemeInLocalStorage = (theme) =>
   localStorage.setItem(localStorageKey, JSON.stringify(theme));
 
 export const getThemeInLocalStorage = () =>
-  JSON.parse(localStorage.getItem(localStorageKey)) || true;
+  JSON.parse(localStorage.getItem(localStorageKey)) || false;
